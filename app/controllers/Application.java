65c871a29d60d5f12951d44841fbbb678febd5fb
tpoperecinii <monomachus85@yahoo.com>
2011-07-07 18:15:41 +0300

    Added initial-data and index and index.html

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,15 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.List;
 
-import java.util.*;
-
-import models.*;
+import models.Post;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
     public static void index() {
-        render();
+        Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
+        render(frontPost, olderPosts);
     }
-
 }
