64f030b380bed4042ba8966e73769b254d880fef
unknown <monomachus85@yahoo.com>
2011-07-07 20:53:54 +0300

    Completed gitignore file

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,9 +3,19 @@ package controllers;
 import java.util.List;
 
 import models.Post;
+import play.Play;
+import play.mvc.Before;
 import play.mvc.Controller;
 
 public class Application extends Controller {
+
+	@Before
+	static void addDefaults() {
+		renderArgs.put("blogTitle",
+				Play.configuration.getProperty("blog.title"));
+		renderArgs.put("blogBaseline",
+				Play.configuration.getProperty("blog.baseline"));
+	}
 
     public static void index() {
         Post frontPost = Post.find("order by postedAt desc").first();
