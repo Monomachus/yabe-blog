7f642e35327cfa2b4c659a7816d169b0efcfc33a
unknown <monomachus85@yahoo.com>
2011-07-09 01:09:12 +0300

    Added Secure module to the equation and also authentification part

--- app/controllers/Comments.java
+++ app/controllers/Comments.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import play.mvc.With;
 
+@With(Secure.class)
 public class Comments extends CRUD {
 
 }
---
+++ app/controllers/Security.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import models.User;
+
+public class Security extends Secure.Security {
+
+	static boolean authenticate(String username, String password) {
+		return User.connect(username, password) != null;
+    }
+
+	static void onDisconnected() {
+	    Application.index();
+	}
+
+	static void onAuthenticated() {
+	    Admin.index();
+	}
+}
