26e3f6384a82f8278311d1e271d96fa2e66226f8
unknown <monomachus85@yahoo.com>
2011-07-09 01:54:50 +0300

    Added roles and styling to administration part

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -4,6 +4,13 @@ import models.User;
 
 public class Security extends Secure.Security {
 
+	static boolean check(String profile) {
+		if ("admin".equals(profile)) {
+			return User.find("byEmail", connected()).<User> first().isAdmin;
+		}
+		return false;
+	}
+
 	static boolean authenticate(String username, String password) {
 		return User.connect(username, password) != null;
     }
